88fc79745293e272215d842328e92cd5fc778fbd
nTrong <55505553+ntrong22@users.noreply.github.com>
2022-04-18 18:08:12 +0700

    Update dangnhap.php

--- dangnhap.php
+++ dangnhap.php
@@ -13,7 +13,7 @@ if(isset($_POST['login']))
         $_SESSION['thongbaolo'] = "Bạn Cần Phải Đăng Nhập";
 		echo "
 							<script language='javascript'>
-								alert('Tài khoản không tồn tại');
+								alert('Tài khoản đăng nhập không đúng');
 								window.open('index.php','_self', 1);
 							</script>
 						";
